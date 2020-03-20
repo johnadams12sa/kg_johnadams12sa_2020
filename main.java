a0e8c7aa6ff8a7a93469b18829bb1c54d5f51729
Aaron Yam <ayam5005@gmail.com>
2020-03-20 16:03:43 -0400

    modification to main class

--- main.java
+++ main.java
@@ -10,8 +10,8 @@ public class main{
 
         boolean result = false;
         
-        Char[] inputString1 = args[0].toCharArray();
-        Char[] inputString2 = args[0].toCharArray();
+        char[] inputString1 = args[0].toCharArray();
+        char[] inputString2 = args[0].toCharArray();
         boolean sizeCheckResult = checkSize(inputString1, inputString2);
         boolean checkDuplicatesResult = checkDuplicates(inputString1);
         if(sizeCheckResult && checkDuplicatesResult){
@@ -28,7 +28,7 @@ public class main{
      * @return Boolean if a one to one relationship can be established, return true,
      * otherwise false
      */
-    public boolean map(Char[] string1, Char[] string2){
+    public static boolean map(char[] string1, char[] string2){
         HashMap association = new HashMap(string1.length);
         int i = 0;
         for(char a : string1){
@@ -42,12 +42,12 @@ public class main{
      * check to see if both input strings are the same length for the one to one
      * relationship
      * 
-     * @param Char[] input1
-     * @param Char[] input2
+     * @param char[] input1
+     * @param char[] input2
      * @return boolean if both input strings are the same length, return true
      * otherwise false
      */
-    public boolean checkSize(Char[] string1, Char[] string2){
+    public static boolean checkSize(char[] string1, char[] string2){
         if(string1.length == string2.length){
             return true;
         }
@@ -60,11 +60,11 @@ public class main{
      * check to see if first input string has any duplicate characters, 
      * if it does, it will return false, otherwise true
      * 
-     * @param Char[] input1
+     * @param char[] input1
      * @return boolean return true if all characters are unique, otherwise
      * false
      */
-    public boolean checkDuplicates(Char[] string1){
+    public static boolean checkDuplicates(char[] string1){
         for(char a : string1){
             for (char b : string1){
                 if(a == b){
