c86720efd9019f767a8ec81432ae58f433b6fce5
Aaron Yam <ayam5005@gmail.com>
2020-03-20 12:14:45 -0400

    added javadoc comments

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 import java.util.HashSet;
 
+/**
+ * @author  Aaron Yam
+ * @version 1.0
+ */
+
 public class main{
     public static void main(String[] args){
         
@@ -12,12 +17,25 @@ public class main{
         //System.out.println("TEST");
     }
 
-    //checks to see if characters can map
+    /**
+     * checks to see if characters can map
+     * @param string input1
+     * @param string input2
+     * @return Boolean if a one to one relationship can be established
+     */
+
     public boolean map(Char[] string1, Char[] string2){
 
     }
-
-    //check to see if both input strings are the same length for the one to one relationship
+    
+    /**
+     * check to see if both input strings are the same length for the one to one
+     * relationship
+     * 
+     * @param string1 input1
+     * @param string2 input2
+     * @return boolean if both input strings are the same length or not
+     */
     public boolean checkSize(Char[] string1, Char[] string2){
         if(string1.length == string2.length){
             return true;
@@ -26,5 +44,6 @@ public class main{
             return false;
         }
     }
+
 
 }
