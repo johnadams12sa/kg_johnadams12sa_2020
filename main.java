cfe61cc80db4a2d1f299ccb278a165a2ee3c9cbc
Aaron Yam <ayam5005@gmail.com>
2020-03-20 11:52:38 -0400

    accept input from cli

--- main.java
+++ main.java
@@ -2,7 +2,11 @@ import java.util.HashSet;
 
 public class main{
     public static void main(String[] args){
-        System.out.println("TEST");
+        
+        Char[] inputString1 = args[0].toCharArray();
+        Char[] inputString2 = args[0].toCharArray();
+        
+        //System.out.println("TEST");
     }
 
 }
