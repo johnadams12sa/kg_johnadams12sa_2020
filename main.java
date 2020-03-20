8369b57c0738c03e20a5d81d4845c971030ab79e
Aaron Yam <ayam5005@gmail.com>
2020-03-20 12:10:19 -0400

    added a test for edge cases

--- main.java
+++ main.java
@@ -5,8 +5,26 @@ public class main{
         
         Char[] inputString1 = args[0].toCharArray();
         Char[] inputString2 = args[0].toCharArray();
-        
+        checkSize(inputString1, inputString2);
+
+        boolean result = map(inputString1, inputString2);
+        System.out.println(result);
         //System.out.println("TEST");
+    }
+
+    //checks to see if characters can map
+    public boolean map(Char[] string1, Char[] string2){
+
+    }
+
+    //check to see if both input strings are the same length for the one to one relationship
+    public boolean checkSize(Char[] string1, Char[] string2){
+        if(string1.length == string2.length){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
